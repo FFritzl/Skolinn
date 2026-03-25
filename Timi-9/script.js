87815c7b35104263beb4a5eab975debcb8fbc02d
FFritzl <95041814+FFritzl@users.noreply.github.com>
2026-03-25 20:19:51 +0000

    Loopur

---
+++ Timi-9/script.js
@@ -0,0 +1,20 @@
+console.log("loopa 1-5");
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+console.log("loopa sem squarar");
+for (let i = 1; i <= 5; i++) {
+  let squared = i * i;
+  console.log(squared);
+}
+
+console.log("loopa 10-0");
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+console.log("loopa frá 0-10 með 2 á milli");
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
